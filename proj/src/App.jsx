bb4ffdd7b6a05fb0b3f542cbfccef6bb81ef39f9
Akhilesh-tirumalasetty <2400032659@kluniversity.in>
2025-12-11 10:35:21 +0530

    first commit

---
+++ proj/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div
+      style={{
+        fontFamily: "sans-serif",
+        textAlign: "center",
+        marginTop: "40px",
+      }}
+    >
+      <h1>Full Stack</h1>
+      <p>Welcome to my Full Stack React app.</p>
+    </div>
+  );
+}
+
+export default App;
